0d57786f3aff46d8bc4daff95fbe2ed117c6a086
shreydan <iamshreydan@gmail.com>
2021-01-27 13:59:13 +0530

    added grid layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Grid from '@react-css/grid'
 
 function App() {
   return (
     <div className="App">
-      
+      <Grid columns='1fr 2fr' rows='1fr 7fr' columnGap='5px'>
+        <Grid.Item column='1/3' className="header">
+        </Grid.Item>
+      </Grid>
     </div>
   );
 }
