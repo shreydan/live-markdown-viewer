678517c0c45aa95fca1404c61c58448baeaf00af
shreydan <iamshreydan@gmail.com>
2021-01-30 23:18:00 +0530

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Grid from '@react-css/grid'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <div className="App">
-      <Grid columns='1fr 2fr' rows='1fr 7fr' columnGap='5px'>
+      <Grid columns='1fr 4fr' rows='0.5fr 4fr' columnGap='5px' style={{'height': '100%'}}>
         <Grid.Item column='1/3' className="header">
+          <Header></Header>
         </Grid.Item>
+        <div className='list'>This is the list component</div>
       </Grid>
     </div>
   );
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='headerComp'>
+            <div>Markdown Notebook</div>
+        </div>
+    )
+}
+
+export default Header
