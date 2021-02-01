8faa0376b8a2281d83bceffca30165c84801bf63
shreydan <iamshreydan@gmail.com>
2021-02-01 13:02:51 +0530

    made toolbar component 0.1

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import './App.css';
 import Grid from '@react-css/grid'
 import Header from './Components/Header/Header'
 import List from './Components/List/List'
+import Toolbar from './Components/ToolBar/Toolbar'
 
 function App() {
   return (
     <div className="App">
-      <Grid columns='1fr 4fr' rows='1fr 0.5fr 8.5fr' columnGap='5px' style={{'height': '100%'}}>
+      <Grid columns='1fr 4fr' rows='0.5fr 0.5fr 9fr' columnGap='5px' style={{'height': '100%'}}>
         <Grid.Item column='1/3' className="header">
           <Header></Header>
         </Grid.Item>
         <Grid.Item column='1/3'>
-          <div className='tool'>This is the tool component</div>
+          <Toolbar title="this is a sample title." Listlength={5}></Toolbar>
         </Grid.Item>
         <List></List>
+        <Grid.Item style={{'backgroundColor': 'black'}}>
+          <div></div>
+        </Grid.Item>
+
       </Grid>
     </div>
   );
--- src/Components/ToolBar/Toolbar.jsx
+++ src/Components/ToolBar/Toolbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import './Toolbar.css'
 
-function Toolbar() {
-    return (
-        <div className='ToolbarComp'>
-            
-        </div>
-    )
+function Toolbar(props) {
+    if(props.Listlength !== 0) {
+        return (
+            <div className='ToolbarComp'>
+                <div id='pageTitle'>{props.title}</div>
+                <div id='editBtn'>Edit</div>
+                <div id='delBtn'>Delete</div>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className='ToolbarComp'>
+                <div id='pageTitle'>Create a new page.</div>
+            </div>
+        )
+    }
+    
 }
 
 export default Toolbar
