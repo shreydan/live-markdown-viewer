b5f8c68b5f9a941488f0081d88168061850b5170
shreydan <iamshreydan@gmail.com>
2021-02-16 17:40:47 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
 import Grid from '@react-css/grid'
-import Header from './Components/Header/Header'
+import Markdown from 'markdown-to-jsx'
+import React, { useState } from 'react'
 
 
 function App() {
+
+  const [md, setMD] = useState("#### Start writing your markdown here...")
+  const updateMDstring = (e) => {
+    e.preventDefault()
+    setMD(e.target.value)
+  }
+
   return (
     <div className="App">
-      <Grid columns='1fr 4fr' rows='0.5fr 0.5fr 9fr' style={{'height': '100%'}}>
+      <Grid columns='1fr 1fr' rows='1fr 11fr' style={{'height': '100%'}}>
         <Grid.Item column='1/3' className="header">
-          <Header></Header>
+          <div className='headerComp'>
+            <div>Markdown</div>
+          </div>
         </Grid.Item>
-
+        <textarea onChange={updateMDstring} 
+                  value = {md}
+                  className='textarea'>
+        </textarea>
+        <Markdown className="md">{md}</Markdown>
       </Grid>
     </div>
   );
